06720717c028a51e9c296367be134784209064a5
muzaffarhaque <muzaffarhaque777@gmail.com>
2025-05-05 01:39:20 +0530

    last testing

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	base: './',
 	plugins: [react()],
+	server: {
+		headers: {
+		  'Content-Type': 'application/javascript',
+		},
+	  },
+	  build: {
+		outDir: 'dist',
+	  },
   });
